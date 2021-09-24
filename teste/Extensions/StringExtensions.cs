1c387d135717d00bbba7dd62305d29495dcfaa20
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-09-23 23:31:25 -0300

    Add project files.

---
+++ teste/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int num)
+        {
+            if (thisObj.Length <= num)
+                return thisObj;
+
+            else
+                return thisObj.Substring(0, num) + "...";
+        }
+    }
+}
---
+++ teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using teste.Extensions;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Vinicius de Souza Nascumento";
+            Console.WriteLine(s1.Cut(8));
+        }
+    }
+}
